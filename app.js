944a727a4f981099db1afdbe44415e5f46c77267
Hanzala Saeed <hanzalasaeed47@gmail.com>
2020-08-26 17:04:37 +0500

    Gave all post, get structure and connected with the client server

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/sign-up.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.mail;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(port, () => {
